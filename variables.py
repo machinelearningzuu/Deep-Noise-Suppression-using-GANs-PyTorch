0fdc29864eaa5570d568730d0dbb30f4d7b7dd23
1zuu <isurualagiyawanna9717@gmail.com>
2021-10-03 18:10:24 +0530

    critic code

---
+++ variables.py
@@ -0,0 +1,12 @@
+resample_rate = 22050
+signal_length = resample_rate
+
+frame_size = 1024
+hop_length = frame_size // 2
+n_mels = 64
+
+learning_rate = 0.001
+epochs = 20
+batch_size = 32
+
+audio_dir = 'data/'
